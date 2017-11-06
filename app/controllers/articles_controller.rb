307fc82bd7ebdd4440bd5d78ea5e0b143d7c5c6c
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-06 07:55:27 +0000

    New update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -57,6 +57,10 @@ class ArticlesController < ApplicationController
     
     private
     
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
     def article_params
         params.require(:article).permit(:title, :description)
     end
