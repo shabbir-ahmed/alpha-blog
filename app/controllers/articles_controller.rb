126f61ce7e30d200f28e188cefb5e0234ffb995a
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-06 07:32:43 +0000

    New update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,14 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    
+        respond_to do |wants|
+            wants.html # index.html.erb
+            wants.xml  { render :xml => @articles }
+        end
+    end
+    
     def new
         @article = Article.new
     end
@@ -15,6 +25,34 @@ class ArticlesController < ApplicationController
     
     def show
         @article = Article.find(params[:id])
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        respond_to do |format|
+            if @article.update_attributes(article_params)
+                flash[:notice] = 'Article was successfully updated.'
+                format.html { redirect_to(@article) }
+                format.xml  { head :ok }
+            else
+                format.html { render :action => "edit" }
+                format.xml  { render :xml => @article.errors, :status => :unprocessable_entity }
+            end
+        end
+    end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+    
+        respond_to do |format|
+            format.html { redirect_to(articles_url) }
+            format.xml  { head :ok }
+        end
     end
     
     private
