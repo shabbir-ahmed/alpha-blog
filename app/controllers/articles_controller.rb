50b63d8773c337064e8eda42d2d88077b81a0337
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-07 07:05:39 +0000

    assoc

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
     end
     
     def create
+       # debugger
         @article = Article.new(article_params)
         if @article.save
             flash[:success] = "Article was successfully created"
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+    belongs_to :user
     validates :title, :description, presence: true
+    validates :user_id, presence: true
 end
