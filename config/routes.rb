dfaa8da383391da1c95563b840243449ce38e15e
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-08 04:59:57 +0000

    Add

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   
   resources :users, except: [:new]
-
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
