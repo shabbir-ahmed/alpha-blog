af7778e200ac43e7dac1264c3c1936f1adad2306
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-08 10:14:03 +0000

    Aout

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
     before_action :require_user, except: [:index, :show]
-    before_action :require_same_user, only: [:edit, :update, :destroy]
+    # before_action :require_same_user, only: [:edit, :update, :destroy]
     
     def index
         @articles = Article.paginate(page: params[:page], per_page: 10).order(id: :desc)
@@ -17,7 +17,8 @@ class ArticlesController < ApplicationController
     
     def create
        # debugger
-        @article = current_user.articles.new(article_params)
+        @article = Article.new(article_params)
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created"
             redirect_to article_path(@article)
@@ -69,11 +70,11 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
     
-    def require_same_user
-        if current_user != @article.user
-            flash[:danger] = "You can only delete your own article"
-            redirect_to root_path
-        end
+    # def require_same_user
+    #     if current_user.id != @article.user
+    #         flash[:danger] = "You can only delete your own article"
+    #         redirect_to root_path
+    #     end
         
-    end
+    # end
 end
