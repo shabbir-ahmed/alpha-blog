d1d44b16fb1aadd2cffe27803f293a849166a0ae
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-11-09 06:34:27 +0000

    Category

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
     belongs_to :user
+    has_many :article_categories
+    has_many :categories, through: :article_categories
     validates :title, :description, presence: true
     validates :user_id, presence: true
 end
